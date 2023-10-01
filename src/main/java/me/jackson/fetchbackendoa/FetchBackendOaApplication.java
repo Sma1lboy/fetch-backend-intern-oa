3786bda9afa8f681ede614881abc68deefca5cf0
Sma1lboy <541898146chen@gmail.com>
2023-09-30 19:10:29 -0500

    init project

---
+++ src/main/java/me/jackson/fetchbackendoa/FetchBackendOaApplication.java
@@ -0,0 +1,13 @@
+package me.jackson.fetchbackendoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchBackendOaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FetchBackendOaApplication.class, args);
+    }
+
+}
